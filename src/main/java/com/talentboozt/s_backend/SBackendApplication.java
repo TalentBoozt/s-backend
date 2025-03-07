d9a2deb6ea52042bb74c093dc65755222404712b
kavi castelo <kavindu123450@gmail.com>
2025-03-07 19:22:28 +0530

    Initial commit

---
+++ src/main/java/com/talentboozt/s_backend/SBackendApplication.java
@@ -0,0 +1,13 @@
+package com.talentboozt.s_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talentboozt/s_backend/SBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentboozt.s_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
