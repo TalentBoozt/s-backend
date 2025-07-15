ee92314cea3cdcd19e308a572d56054cbc828a3b
kavi castelo <kavindu123450@gmail.com>
2025-07-15 07:14:20 +0530

    fix array outofbound exception

--- src/main/java/com/talentboozt/s_backend/domains/user/repository/EmpCertificatesRepository.java
+++ src/main/java/com/talentboozt/s_backend/domains/user/repository/EmpCertificatesRepository.java
@@ -4,8 +4,10 @@ import com.talentboozt.s_backend.domains.user.model.EmpCertificatesModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmpCertificatesRepository extends MongoRepository<EmpCertificatesModel, String> {
-    List<EmpCertificatesModel> findByEmployeeId(String employeeId);
+    List<EmpCertificatesModel> findAllByEmployeeId(String employeeId);
+    Optional<EmpCertificatesModel> findByEmployeeId(String employeeId);
     void deleteByEmployeeId(String employeeId);
 }
