c23f9f1fd6cddd737cf20349e03c914d107e2011
kavi castelo <kavindu123450@gmail.com>
2025-08-05 13:27:20 +0530

    exposing a public path

---
+++ src/main/java/com/talentboozt/s_backend/config/StaticResourceConfig.java
@@ -0,0 +1,21 @@
+package com.talentboozt.s_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Paths;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String externalPath = Paths.get("docs/").toAbsolutePath().toUri().toString();
+
+        registry.addResourceHandler("/docs/**")
+                .addResourceLocations(externalPath)
+                .setCachePeriod(3600);
+    }
+}
+
