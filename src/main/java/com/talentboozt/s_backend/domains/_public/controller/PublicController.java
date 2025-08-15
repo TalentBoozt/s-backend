7d572eaed4ac6bbed51f1a31a2039a3538010501
kavi castelo <kavindu123450@gmail.com>
2025-08-15 23:46:34 +0530

    fix json error

--- src/main/java/com/talentboozt/s_backend/domains/_public/controller/PublicController.java
+++ src/main/java/com/talentboozt/s_backend/domains/_public/controller/PublicController.java
@@ -2,6 +2,7 @@ package com.talentboozt.s_backend.domains._public.controller;
 
 import com.talentboozt.s_backend.domains._public.service.PublicService;
 import com.talentboozt.s_backend.domains.com_courses.dto.CourseMissedNotify;
+import com.talentboozt.s_backend.domains.common.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +17,7 @@ public class PublicController {
     }
 
     @PostMapping("/add/batch-notification/{courseId}")
-    public ResponseEntity<?> addBatchNotification(@PathVariable String courseId, @RequestBody CourseMissedNotify courseMissedNotify) {
+    public ResponseEntity<ApiResponse> addBatchNotification(@PathVariable String courseId, @RequestBody CourseMissedNotify courseMissedNotify) {
         return publicService.addNewBatchNotification(courseId, courseMissedNotify);
     }
 }
