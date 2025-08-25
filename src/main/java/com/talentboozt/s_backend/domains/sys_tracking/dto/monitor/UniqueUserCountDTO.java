9b8072a712b11e8dace22055569ad0dabe9ea612
kavi castelo <kavindu123450@gmail.com>
2025-08-25 22:44:17 +0530

    fix documents direct parse error

---
+++ src/main/java/com/talentboozt/s_backend/domains/sys_tracking/dto/monitor/UniqueUserCountDTO.java
@@ -0,0 +1,13 @@
+package com.talentboozt.s_backend.domains.sys_tracking.dto.monitor;
+
+public class UniqueUserCountDTO {
+    private int uniqueUserCount;
+
+    public int getUniqueUserCount() {
+        return uniqueUserCount;
+    }
+
+    public void setUniqueUserCount(int uniqueUserCount) {
+        this.uniqueUserCount = uniqueUserCount;
+    }
+}
