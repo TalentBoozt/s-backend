3460509b420e47022c1eec07ea394a38002fabe1
kavi castelo <kavindu123450@gmail.com>
2025-08-25 21:29:10 +0530

    Update MonitoringController.java

--- src/main/java/com/talentboozt/s_backend/domains/sys_tracking/controller/monitor/MonitoringController.java
+++ src/main/java/com/talentboozt/s_backend/domains/sys_tracking/controller/monitor/MonitoringController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -31,9 +32,11 @@ public class MonitoringController {
     @GetMapping("/overview")
     public DashboardOverviewDTO getOverview(
             @RequestParam String trackingId,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant from,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant to) {
-        return monitoringService.getOverview(trackingId, from, to);
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime to) {
+        Instant fromInstant = from.toInstant();
+        Instant toInstant = to.toInstant();
+        return monitoringService.getOverview(trackingId, fromInstant, toInstant);
     }
 
     @GetMapping("/page-views")
